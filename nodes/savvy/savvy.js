e141e454d196e2e32b06b0893a38ddc25946782f
nicolasferry <nicolas.ferry@sintef.no>
2018-02-16 21:06:32 +0100

    Improve savvy node

--- nodes/savvy/savvy.js
+++ nodes/savvy/savvy.js
@@ -12,19 +12,23 @@ module.exports = function (RED) {
         // Store local copies of the node configuration (as defined in the .html)
         this.topic = n.topic;
         this.endpoint = n.endpoint;
+        this.key=n.key;
+        this.target=n.target;
+        this.locationid=n.locationid;
+        this.machineid=n.machineid;
+        this.groupid=n.groupid;
 
         // copy "this" object in case we need it in context of callbacks of other functions.
         var node = this;
 
         // respond to inputs....
         this.on('input', function (msg) {
-
-            sendGet(this.endpoint, function (res) {
+            var method=computeEndpoint(this.target, node);
+            sendGet(method, node, function (res) {
                 var msg = {};
                 msg.payload = res;
                 node.send(msg);
             });
-
         });
 
         this.on("close", function () {
@@ -35,13 +39,33 @@ module.exports = function (RED) {
     }
 
 
-    function sendGet(path, callback) {
-        var epoch = new Date().getTime();
-        var auth = generateAuthorization(path, epoch);
+    function computeEndpoint(target, node){
+        var ep="";
+        switch(target) {
+            case "stream":
+                ep+="/v1/stream?track="+node.machineid;
+                break;
+            case "machines":
+                ep+="/v1/locations/"+node.locationid+"/machines";
+                break;
+            case "indicators":
+                ep+="/v1/locations/"+node.locationid+"/machines/"+node.machineid+"/groups/"+node.groupid+"/indicators";
+                break;
+            case "groups":
+                ep+="/v1/locations/"+node.locationid+"/machines/"+node.machineid+"/groups";
+                break;
+            default:
+                ep+="/v1/locations/";
+        }
+        return ep;
+    }
 
-	//TODO: get host from configuration
+    function sendGet(path, node, callback) {
+        var epoch = new Date().getTime();
+        var auth = generateAuthorization(path, epoch, node.key);
+
         var options = {
-            host: "XXXX",
+            host: node.endpoint,
             path: path,
             method: "GET",
             "headers": {
@@ -77,11 +101,11 @@ module.exports = function (RED) {
 
 
 
-    function generateAuthorization(loc, epoch) {
+    function generateAuthorization(loc, epoch, key) {
         var Request = "GET" + "\n" + "text/plain; charset=UTF-8" + "\n" + epoch + "\n" + loc;
 
 	//TODO: get credentials from configuration
-        var authorization = "M2C" + " " + "XXX" + ":" + generateHmac(Request, "XXX");
+        var authorization = "M2C" + " " + "xptbQCSLWL" + ":" + generateHmac(Request, key);
 
         return authorization;
     }
