4e3627be264a878d28c643f2613a10b49d1dbfb5
nicolasferry <nicolas.ferry@sintef.no>
2018-02-22 15:35:14 +0100

    Updates from IDEKO: I’ve added a “API Type” to the input menu, so now you can select between the two types of the Savvy APIs; I’ve added the “Return” type to input menu. Now you can get the results in a UTF-8 string or in a JSON object.; I’ve added the key and secret to the input menu and changed that function.; Changed some of code on the response of a stream; The input menu options now hides/shows the input blocks depending the option you’ve selected.

--- nodes/savvy/savvy.js
+++ nodes/savvy/savvy.js
@@ -1,9 +1,8 @@
 module.exports = function (RED) {
     "use strict";
 
-
     var crypto = require("crypto");
-    var http = require('https');
+    var https = require('https');
 
     function savvyNode(n) {
         // Create a RED node
@@ -12,21 +11,33 @@ module.exports = function (RED) {
         // Store local copies of the node configuration (as defined in the .html)
         this.topic = n.topic;
         this.endpoint = n.endpoint;
-        this.key=n.key;
-        this.target=n.target;
-        this.locationid=n.locationid;
-        this.machineid=n.machineid;
-        this.groupid=n.groupid;
+		this.returntype = n.returntype;
+		this.apitype = n.apitype;
+		this.port = n.port;
+        this.key = n.key;
+		this.secret = n.secret;
+        this.target = n.target;
+        this.locationid = n.locationid;
+        this.machineid = n.machineid;
+        this.groupid = n.groupid;
 
         // copy "this" object in case we need it in context of callbacks of other functions.
         var node = this;
 
         // respond to inputs....
         this.on('input', function (msg) {
-            var method=computeEndpoint(this.target, node);
-            sendGet(method, node, function (res) {
+			
+            var method = computeEndpoint(node);
+            sendGet(msg, method, node, function (res) {
                 var msg = {};
-                msg.payload = res;
+				switch(node.returntype) {
+					case "utf-8":
+						msg.payload = res;
+						break;
+					case "json":
+						msg.payload = JSON.parse(res);
+						break;
+				}
                 node.send(msg);
             });
         });
@@ -38,47 +49,85 @@ module.exports = function (RED) {
         });
     }
 
-
-    function computeEndpoint(target, node){
-        var ep="";
-        switch(target) {
-            case "stream":
-                ep+="/v1/stream?track="+node.machineid;
-                break;
-            case "machines":
-                ep+="/v1/locations/"+node.locationid+"/machines";
-                break;
-            case "indicators":
-                ep+="/v1/locations/"+node.locationid+"/machines/"+node.machineid+"/groups/"+node.groupid+"/indicators";
-                break;
-            case "groups":
-                ep+="/v1/locations/"+node.locationid+"/machines/"+node.machineid+"/groups";
-                break;
-            default:
-                ep+="/v1/locations/";
-        }
+    function computeEndpoint(node) {
+        var ep = "";
+		
+		if (node.apitype === 'cloud')
+		{
+			switch(node.target)
+			{
+				case "stream":
+					ep += "/v1/stream?track=" + node.machineid;
+					break;
+				case "machines":
+					ep += "/v1/locations/" + node.locationid + "/machines";
+					break;
+				case "indicators":
+					ep += "/v1/locations/" + node.locationid + "/machines/" + node.machineid + "/groups/" + node.groupid + "/indicators";
+					break;
+				case "groups":
+					ep += "/v1/locations/" + node.locationid + "/machines/" + node.machineid + "/groups";
+					break;
+				default:
+					ep += "/v1/locations/";
+			}
+		}
+		else if (node.apitype === 'local')
+		{
+			ep += "/stream?machines=" + node.machineid;
+		}
+		
         return ep;
     }
 
-    function sendGet(path, node, callback) {
+    function sendGet(msg, path, node, callback) {
         var epoch = new Date().getTime();
-        var auth = generateAuthorization(path, epoch, node.key);
+        var auth = generateAuthorization(path, epoch, node.key, node.secret);
 
-        var options = {
-            host: node.endpoint,
-            path: path,
-            method: "GET",
-            "headers": {
-                "Content-Type": "text/plain; charset=UTF-8",
-                "X-M2C-Sequence": epoch,
-                "Authorization": auth
-            }
-        };
+		switch(node.apitype) 
+		{
+			case "cloud":
+				var options = {
+					host: node.endpoint,
+					path: path,
+					method: "GET",
+					"headers": {
+						"Content-Type": "text/plain; charset=UTF-8",
+						"X-M2C-Sequence": epoch,
+						"Authorization": auth
+					}
+				};
+				break;
+				
+			// Local restStreaming requires a port and doesn't need extra headers
+			case "local":
+				var options = {
+					host: node.endpoint,
+					port: node.port,
+					path: path,
+					method: "GET",
+					"headers": {
+						"Content-Type": "text/plain; charset=UTF-8"
+					}
+				};
+				break;
+		}
 
-        var req = http.request(options, function (response) {
+        var req = https.request(options, function (response) {
+			
             var str = ''
             response.on('data', function (chunk) {
-                str += chunk;
+			
+				// If we are streaming (cloud or local), we return the chunk itself as the full response is in one chunk
+				if (node.target === "stream" || node.apitype === 'local')
+				{	
+					// The '' + chunk is added to convert the buffer into a string, and the result is always on the pos [1] of the splitted chunk. 
+					callback(('' + chunk).split("\r\n")[1]);			
+				}					
+				else
+				{
+					str += chunk;
+				}					
             });
 
             response.on('end', function () {
@@ -88,33 +137,29 @@ module.exports = function (RED) {
             response.on('error', function (e) {
                 console.log(e);
             });
-
         });
-
+		
         req.on('error', function (err) {
             console.log("Connection not open " + err);
-
         });
 
         req.end();
     }
 
-
-
-    function generateAuthorization(loc, epoch, key) {
+    function generateAuthorization(loc, epoch, key, secret)
+	{
         var Request = "GET" + "\n" + "text/plain; charset=UTF-8" + "\n" + epoch + "\n" + loc;
-
-	//TODO: get credentials from configuration
-        var authorization = "M2C" + " " + "xptbQCSLWL" + ":" + generateHmac(Request, key);
+        var authorization = "M2C" + " " + key + ":" + generateHmac(Request, secret);
 
         return authorization;
     }
 
-
-    function generateHmac(data, key, algorithm, encoding) {
+    function generateHmac(data, secret, algorithm, encoding)
+	{
         var encoding = encoding || "base64";
         var algorithm = algorithm || "sha1";
-        return crypto.createHmac(algorithm, key).update(data).digest(encoding);
+
+        return crypto.createHmac(algorithm, secret).update(data).digest(encoding);
     }
 
     // Register the node by name. This must be called before overriding any of the
